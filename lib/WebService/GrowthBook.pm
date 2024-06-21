13efcebf19b1cee8e1add666c42f52bf17f08690
chylli-deriv <chylli@deriv.com>
2024-06-21 21:18:12 +0800

    init

---
+++ lib/WebService/GrowthBook.pm
@@ -0,0 +1,36 @@
+package WebService::GrowthBook;
+# ABSTRACT: ...
+
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+=head1 NAME
+
+WebService::GrowthBook - Module abstract
+
+=head1 SYNOPSIS
+
+    use WebService::GrowthBook;
+    my $instance = WebService::GrowthBook->new;
+
+=head1 DESCRIPTION
+
+=cut
+
+=head1 METHODS
+
+=cut
+
+1;
+
+
+=head1 SEE ALSO
+
+=over 4
+
+=item *
+
+=back
+
