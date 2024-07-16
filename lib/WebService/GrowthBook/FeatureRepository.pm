6e7809baaeaa866f3aa0d2d1342cea0597bf67db
youssef-deriv <youssef@regentmarkets.com>
2024-07-16 07:26:10 +0000

    Move client_key to the request body

--- lib/WebService/GrowthBook/FeatureRepository.pm
+++ lib/WebService/GrowthBook/FeatureRepository.pm
@@ -36,54 +36,52 @@ class WebService::GrowthBook::FeatureRepository {
             $cache->set($key, $features, $ttl);
             $log->debug("Features loaded from GrowthBook API, set in cache");
         }
-        
         return $features;
     }
 
     method _fetch_features($api_host, $client_key){
         my $decoded = $self->_fetch_and_decode($api_host, $client_key);
-        
+
         # TODO decrypt here
         if(exists $decoded->{features}){
             return $decoded->{features};
         }
         else {
             $log->warn("GrowthBook API response missing features");
-            return
+            return;
         }
-    }    
-    
-    method _fetch_and_decode($api_host, $client_key){
+    }
+
+    method _fetch_and_decode($api_host, $client_key) {
         try {
-            my $r = $self->_get($self->_get_features_url($api_host), $client_key);
-            if($r->{status} >= 400){
+            my $r = $self->_get($self->_get_features_url($api_host, $client_key));
+            if ($r->{status} >= 400) {
                 $log->warnf("Failed to fetch features, received status code %d", $r->{status});
                 return;
             }
             my $decoded = decode_json_utf8($r->{content});
             return $decoded;
-        }
-        catch ($e){
+        } catch ($e) {
             $log->warnf("Failed to decode feature JSON from GrowthBook API: %s", $e);
         }
         return;
     }
-    method _get($url, $client_key){
 
+    method _get($url) {
         my $headers = {
-            'Authorization' => "Bearer $client_key",
             'Content-Type'  => 'application/json',
         };
 
-        return  $http->get($url, {headers => $headers});
+        return $http->get($url, {headers => $headers});
     }
 
-    method _get_features_url($api_host){
-        return "$api_host/features";
+    method _get_features_url($api_host, $client_key) {
+        return "$api_host/api/features/$client_key";
     }
 }
 
-sub get_cache_key($api_host, $client_key){
+sub get_cache_key ($api_host, $client_key) {
     return md5_base64($api_host . '::' . $client_key);
 }
 1;
+
